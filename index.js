ba5f34226b496688cb7df79abb69127854824a74
Luis Escobar <lae311@stern.nyu.edu>
2021-10-14 16:10:18 -0400

    commit tree ab5fb1b218b26e39927f956445de96710f92e9a6

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+// function loopThroughAndFind (record, finder) {
+//     let result = null
+//     for (let item of record){
+//         if(item === 'W'){
+//             result = item
+//             break;
+//         }   
+//     }
+//     return result
+// }
+
+
+function superbowlWin(record) {
+    let year = record.find(findWinnerYear);
+    if (year === undefined){
+        return undefined;
+    }
+    else {
+        return year.year;
+    }
+}
+
+function findWinnerYear(record) {
+    return record.result === 'W';
+}
 
